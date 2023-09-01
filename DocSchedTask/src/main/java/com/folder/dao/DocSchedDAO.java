3201bbb5261ed74d979b19fb6590ea8f5790a3da
Mohan <137370382+Amohan1218@users.noreply.github.com>
2023-09-01 06:24:31 +0530

    delete

--- DocSchedTask/src/main/java/com/folder/dao/DocSchedDAO.java
+++
@@ -1,9 +0,0 @@
-package com.folder.dao;
-
-import java.util.List;
-
-import com.folder.model.DoctorSchedule;
-
-public interface DocSchedDAO {
-	boolean createDoctorSchedule(List<DoctorSchedule> L);
-}
